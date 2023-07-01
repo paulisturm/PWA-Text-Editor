87ba40e8f1385c9b8290d451ecf9bdcf4b97cca8
paulisturm <paulisturm22@gmail.com>
2023-07-01 10:36:58 -0400

    install-database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -16,6 +16,16 @@ const initdb = async () =>
 export const putDb = async (content) => console.error('putDb not implemented');
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+console.log('GET from the database');
+const jateDb = await openDB('jate', 1);
+const tx = contactDb.transaction('contact', 'readonly');
+const store = tx.objectStore('contact');
+const request = store.getAll();
+const result = await request;
+  console.log('result.value', result);
+  return result;
+}; 
+
 
 initdb();
