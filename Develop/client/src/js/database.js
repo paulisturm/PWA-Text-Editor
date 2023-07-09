0e78f3794e8d4edafc48c82959e4e9f66f6034be
paulisturm <paulisturm22@gmail.com>
2023-07-09 13:30:38 -0400

    fixed bug

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -17,7 +17,7 @@ export const putDb = async (content) => {
 console.log('Put to the database');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readwrite');
-const store = tx.objectStore('contact');
+const store = tx.objectStore('jate');
 const request = store.add({ id: 1, value: content });
 const result = await request;
   console.log('🚀 - data saved to the database', result.value);
@@ -26,12 +26,12 @@ const result = await request;
 export const getDb = async () => {
 console.log('GET from the database');
 const jateDb = await openDB('jate', 1);
-const tx = contactDb.transaction('jate', 'readonly');
+const tx = jateDb.transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
-const request = store.getAll();
+const request = store.get(1);
 const result = await request;
-  console.log('result.value', result);
-  return result;
+  console.log('result.value', result.value);
+  return result?.value;
 }; 
 
 
